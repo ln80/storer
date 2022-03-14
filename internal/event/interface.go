ec1761995e4dd121e576b5437afc60932924b5c9
Reda Laanait <laanait.reda@gmail.com>
2022-03-14 17:01:25 +0100

    rename internal event file to inteface.go

--- internal/event/interface.go
+++ internal/event/interface.go
@@ -6,6 +6,7 @@ import (
 	"github.com/redaLaanait/storer/event"
 )
 
+// Persister presents the service that persist chunks in the durable store e.g S3
 type Persister interface {
 	Persist(ctx context.Context, stmID event.StreamID, evts event.Stream) error
 }
